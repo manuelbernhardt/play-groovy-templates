e3c539f3d04c07565a24ffe776922e08baadc32f
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-02-21 12:45:19 +0100

    Publishing configuration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Groovy template engine for the Play! framework
 
 This is the original template engine of Play 1, separated so it can be integrated with Play 2.
+
+Published under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
---
+++ project/ProjectBuild.scala
@@ -0,0 +1,40 @@
+import sbt._
+import Keys._
+
+object ProjectBuild extends Build {
+
+  val buildVersion = "0.1-SNAPSHOT"
+
+  val delvingReleases = "Delving Releases Repository" at "http://development.delving.org:8081/nexus/content/groups/public"
+  val delvingSnapshots = "Delving Snapshot Repository" at "http://development.delving.org:8081/nexus/content/repositories/snapshots"
+  val delvingRepository = if (buildVersion.endsWith("SNAPSHOT")) delvingSnapshots else delvingReleases
+
+  lazy val root = Project(
+    id = "groovy-templates-engine",
+    base = file(".")
+  ).settings(
+
+    organization := "eu.delving",
+
+    version := buildVersion,
+
+    resolvers += "jahia" at "http://maven.jahia.org/maven2",
+
+    libraryDependencies ++= Seq(
+      "org.codehaus.groovy"       % "groovy"              % "1.8.5",
+      "commons-collections"       % "commons-collections" % "3.2.1",
+      "commons-lang"              % "commons-lang"        % "2.6",
+      "com.jamonapi"              % "jamon"               % "2.7"
+    ),
+
+    publishTo := Some(delvingRepository),
+
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+
+    publishMavenStyle := true,
+
+    crossPaths := false
+
+  )
+
+}
