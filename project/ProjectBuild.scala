cbd50b7661878bc1cc91b53d4a5f4544223d17cb
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-02-04 10:20:19 +0100

    0.7.5 - upgrade to Groovy 2.0

--- project/ProjectBuild.scala
+++ project/ProjectBuild.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object ProjectBuild extends Build {
 
-  val buildVersion = "0.7.1"
+  val buildVersion = "0.7.5"
 
   val delvingReleases = "Delving Releases Repository" at "http://development.delving.org:8081/nexus/content/repositories/releases"
   val delvingSnapshots = "Delving Snapshot Repository" at "http://development.delving.org:8081/nexus/content/repositories/snapshots"
@@ -19,9 +19,10 @@ object ProjectBuild extends Build {
     version := buildVersion,
 
     resolvers += "jahia" at "http://maven.jahia.org/maven2",
+    resolvers += "codehaus" at "http://repository.codehaus.org/",
 
     libraryDependencies ++= Seq(
-      "org.codehaus.groovy"       % "groovy"              % "1.8.5",
+      "org.codehaus.groovy"       % "groovy-all"          % "2.0.0",
       "commons-collections"       % "commons-collections" % "3.2.1",
       "commons-lang"              % "commons-lang"        % "2.6",
       "com.jamonapi"              % "jamon"               % "2.7"
